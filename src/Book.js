8183e9c35e30a84fe7c844577d873be10c07da58
tedminht90 <minhted@gmail.com>
2021-09-03 12:45:28 +0700

    03/09/2021

--- src/Book.js
+++ src/Book.js
@@ -1,14 +1,31 @@
-const Book = (props) => {
+const Book = ({ img, title, author}) => { //Do sử dụng ...book bên index.js nên sẽ gọi trực tiếp đc không cần qua props
    //const {img, title, author} = props.book;
-    const img = props.book.img;
-    const title = props.book.title;
-    const author = props.book.author;
+   //const img = props.img;
+   //const title = props.title;
+   //const author = props.author;
    //console.log(props.book);
+
+   // attribute, eventHandler
+   // onClick, onMouseOver
+
+   const clickHander = (e) =>{
+     console.log(e);
+     console.log(e.target);
+     alert('hello world');//check xem có hoạt động hay không
+   };
+   const complexExample = (author) =>{
+     console.log(author);
+    }
+
   return(
-      <article className="book">
+      <article className="book" onMouseOver={()=>{
+        console.log(author);
+      }} >
         <img src={img} alt=''/>
-        <h1>{title}</h1>
+        <h1 onClick={() => console.log(title)}>{title}</h1>
         <h4 className="author">{author}</h4>
+        <button type="button" onClick={clickHander}>Reference example</button>
+        <button type="button" onClick={() => complexExample(author)}>More complex example</button>
       </article>
    );
  }
--- src/index.js
+++ src/index.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 import Book from './Book';
-import './index.css'
+import './index.css';
+import {greeting} from '../testing/testing';
 
 //setup vars
-const books = [ //Mảng để lưu trữ các quyển sách
-  {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/51SuFuFRPIL._AC_SX184_.jpg',
-    title: 'Little Blue Truck board book',
-    author: 'Alice Schertle, Jill McElmurry'
-  },
-  {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/51o4b5AdNLL._AC_SX184_.jpg',
-    title: 'Where\'s Spot?',
-    author: 'Eric Hill'
-  },
-   {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/51PRQuO-xjL._AC_SX184_.jpg',
-    title: 'If Animals Kissed Good Night',
-    author: 'Ann Whitford Paul, David Walker'
-  }
-];
+import data from './BookData';
 
 function BookList(){
+  console.log(greeting);
   return (
   <section className="book-list">
-    {books.map((book) => { //Tìm kiềm các phần tử của mảng books lấy ra img, title,author lọc lấy img, title, author
+    {data.map((book) => { //Tìm kiềm các phần tử của mảng books lấy ra img, title,author lọc lấy img, title, author
       //console.log(book);// check xem đã có nhận dữ liệu hay chưa
-      return ( 
-        <Book book={book}></Book>
-      );
+      return (<Book key={book.id} {...book}></Book>); //Viết ...book thay thế cho book={book}
     })}
     </section>
   );
